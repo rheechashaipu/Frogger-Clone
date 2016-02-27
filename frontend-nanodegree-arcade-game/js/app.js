ec3b8f2e0cfbdbac0ebf320a3fc75b68208583c0
Richard Shipe <rheechashaipu@gmail.com>
2016-02-27 13:25:34 -0500

    completed project, basic stuff done so far. will submit and then make adjustments per the code reviewer's suggestions.
    Things I need to consider:
    enlarge map
    add different enemies

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -6,6 +6,7 @@ var Enemy = function(x,y,speed) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    // bug dimensions are 101 width and 171 height
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
@@ -14,13 +15,32 @@ var Enemy = function(x,y,speed) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x+this.speed*dt;
     this.y = this.y;
+    //Reset bug location after moving off game board
+    if(this.x > 510){
+      this.x = -70;
+      this.speed = Math.floor(Math.random() * (180 - 60 + 1)) + 60;;
+    };
+    this.collision();
 };
+//Credit for collision method goes to Udacity member mcs and Mozilla's 2D game tutorial
+
+Enemy.prototype.collision = function(){
+  if(this.x > player.x - 50 && 
+    this.x < player.x + 50 && 
+    this.y > player.y - 40 &&
+    this.y < player.y + 40){
+    player.x = 200;
+    player.y = 400;
+  };
+};
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -39,8 +59,11 @@ var Player = function(x,y) {
 };
 
 Player.prototype.update = function() {
-
-
+  //Resets player's location once player reaches river
+  if(this.y <= 0){
+    this.x = 200;
+    this.y = 400;
+  };
 };
 
 Player.prototype.render = function(){
@@ -76,15 +99,15 @@ Player.prototype.handleInput = function(keyCodes){
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var enemy1 = new Enemy(0,43+100,50);
-allEnemies.push(enemy1);
 
+for(i=0;i<3;i++){
+  var randSpeed = Math.floor(Math.random() * (180 - 60 + 1)) + 60;
+  allEnemies.push(new Enemy(-50,43+i*90, randSpeed));
+};
 
-
+//instantiate player object
 
 var player = new Player(200,400);
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -95,6 +118,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
